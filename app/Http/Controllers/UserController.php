ea84b1945363b1dcf99e254826f94b4d6fcfef22
triyanadewi <triyanadewifatmawati@gmail.com>
2024-03-07 15:53:42 +0700

    Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -108,7 +108,7 @@ class UserController extends Controller
         );
         return view('user', ['data' => $user]);*/
 
-        $user = UserModel::firstOrNew(
+        /*$user = UserModel::firstOrNew(
             [
                 'username' => 'manager33',
                 'nama' => 'Manager Tiga Tiga',
@@ -117,6 +117,47 @@ class UserController extends Controller
             ],
         );
         $user->save();
-        return view('user', ['data' => $user]);
+        return view('user', ['data' => $user]);*/
+
+        /*$user = UserModel::create([
+            'username' => 'manager55',
+            'nama' => 'Manager55',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = "manager56";
+
+        $user->isDirty(); //true
+        $user->isDirty('username'); //true
+        $user->isDirty('nama'); //false
+        $user->isDirty(['nama', 'username']); //true
+
+        $user->isClean(); //false
+        $user->isClean('username'); //false
+        $user->isClean('nama'); //true
+        $user->isClean(['nama', 'username']); //false
+
+        $user->save();
+
+        $user->isDirty(); //false
+        $user->isClean(); //true
+        dd($user->isDirty());*/
+
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = "manager12";
+
+        $user->save();
+
+        $user->wasChanged(); //true
+        $user->wasChanged('username'); //true
+        $user->wasChanged('nama'); //false
+        dd($user->wasChanged(['nama', 'username'])); //true
     }
 }
