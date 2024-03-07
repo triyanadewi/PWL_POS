7948107f5704597faed0104d52598fc014fdc09e
triyanadewi <triyanadewifatmawati@gmail.com>
2024-03-07 13:04:53 +0700

    Properti $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,13 +19,35 @@ class UserController extends Controller
         ];
         UserModel::insert($data); // Tambahkan data ke tabel m_user*/ 
 
-        $data = [
+        /*$data = [
             'nama' => 'Pelanggan Pertama',
         ];
         UserModel::where('username', 'customer-1')->update($data); // Update data user
 
         // Coba akses model UserModel
         $user = UserModel::all(); // Ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);*/
+
+        /*$data = [
+            'level_id' => 2,
+            'username' => 'manager_dua',
+            'nama' => 'Manager 2',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);*/
+
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+
+        $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
 }
