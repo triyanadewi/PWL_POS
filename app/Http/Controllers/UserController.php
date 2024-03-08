da629683b47df5bc1ef6f3cf05fd1353afc040ef
triyanadewi <triyanadewifatmawati@gmail.com>
2024-03-09 00:38:27 +0700

    Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -160,7 +160,13 @@ class UserController extends Controller
         $user->wasChanged('nama'); //false
         dd($user->wasChanged(['nama', 'username'])); //true*/
 
-        $user = UserModel::all();
+        /*$user = UserModel::all();
+        return view('user', ['data' => $user]);*/
+
+        /*$user = UserModel::with('level')->get();
+        dd($user);*/
+
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
     }
 
